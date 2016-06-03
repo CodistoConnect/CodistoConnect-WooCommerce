acb6995b7518e9f458461b37a8f3803d863b146f
Luke Amery <lamery@ontech.com.au>
2016-06-03 10:00:12 +1000

    version bump for new release

--- connect.php
+++ connect.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @package MarketPlace Connect by Codisto
- * @version 1.2.26
+ * @version 1.2.27
  */
 /*
 Plugin Name: MarketPlace Connect by Codisto
 Plugin URI: http://wordpress.org/plugins/codistoconnect/
 Description: WooCommerce eBay Integration - Convert a WooCommerce store into a fully integrated eBay store in minutes
 Author: Codisto
-Version: 1.2.26
+Version: 1.2.27
 Author URI: https://codisto.com/
 License: GPLv2
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
@@ -20,7 +20,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 include_once( ABSPATH . 'wp-admin/includes/file.php' );
 include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 
-define('CODISTOCONNECT_VERSION', '1.2.26');
+define('CODISTOCONNECT_VERSION', '1.2.27');
 define('CODISTOCONNECT_RESELLERKEY', '');
 
 
