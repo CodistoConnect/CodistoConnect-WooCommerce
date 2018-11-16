31ef1e1c76dc028be9c180ea33c4f0b1f78ef99f
jlivas <jlivas@ontech.com.au>
2018-11-16 20:30:10 +1100

    account create fix

--- connect.php
+++ connect.php
@@ -5,7 +5,7 @@
  * Description: WooCommerce Amazon & eBay Integration - Convert a WooCommerce store into a fully integrated Amazon & eBay store in minutes
  * Author: Codisto
  * Author URI: https://codisto.com/
- * Version: 1.3.19
+ * Version: 1.3.20
  * Text Domain: codisto-linq
  * Woo: 3545890:ba4772797f6c2c68c5b8e0b1c7f0c4e2
  * WC requires at least: 2.0.0
@@ -14,14 +14,14 @@
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  *
  * @package Codisto LINQ by Codisto
- * @version 1.3.19
+ * @version 1.3.20
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-define( 'CODISTOCONNECT_VERSION', '1.3.19' );
+define( 'CODISTOCONNECT_VERSION', '1.3.20' );
 define( 'CODISTOCONNECT_RESELLERKEY', '' );
 
 if ( ! class_exists( 'CodistoConnect' ) ) :
@@ -2134,6 +2134,7 @@ final class CodistoConnect {
 										'version' => $blogversion,
 										'url' => $blogurl,
 										'email' => $signupemail,
+										'phone' => $signupphone,
 										'storename' => $blogdescription ,
 										'resellerkey' => $this->reseller_key(),
 										'codistoversion' => CODISTOCONNECT_VERSION
