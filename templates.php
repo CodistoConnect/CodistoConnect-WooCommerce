d51f501b958b904edceb46ac22ae9fd2b0496b28
James Livas <jlivas@ontech.com.au>
2019-08-02 11:53:29 +1000

    multisite + remove sqlite dependancy in template synchronisation

--- templates.php
+++ templates.php
@@ -100,7 +100,16 @@ if( file_exists( $file ) ) {
 				submit_button( __( 'Update Template', 'codisto-linq' ), 'primary', 'submit', true );
 			} else {
 				?>
-				<p><em><?php echo wp_kses( __('You need to make this file writable before you can save your changes. See <a href="https://codex.wordpress.org/Changing_File_Permissions">the Codex</a> for more information.', 'codisto-linq' ) ); ?></em></p>
+				<p><em><?php echo
+					wp_kses(
+						__('You need to make this file writable before you can save your changes. See <a href="https://codex.wordpress.org/Changing_File_Permissions">the Codex</a> for more information.', 'codisto-linq'),
+						array(
+							'a' => array(
+								'class' => array(),
+								'href' => array()
+							)
+						)
+					); ?></em></p>
 				<?php
 			}
 
