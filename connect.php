c755e6d6de7293ab90b2bac414c2110675f35001
James Livas <jlivas@ontech.com.au>
2019-12-23 11:55:53 +1100

    change woocommerce plugin detection

--- connect.php
+++ connect.php
@@ -5,23 +5,23 @@
  * Description: WooCommerce Amazon & eBay Integration - Convert a WooCommerce store into a fully integrated Amazon & eBay store in minutes
  * Author: Codisto
  * Author URI: https://codisto.com/
- * Version: 1.3.33
+ * Version: 1.3.34
  * Text Domain: codisto-linq
  * Woo: 3545890:ba4772797f6c2c68c5b8e0b1c7f0c4e2
  * WC requires at least: 2.0.0
- * WC tested up to: 3.8
+ * WC tested up to: 3.8.1
  * License: GPLv2
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  *
  * @package Codisto LINQ by Codisto
- * @version 1.3.33
+ * @version 1.3.34
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-define( 'CODISTOCONNECT_VERSION', '1.3.33' );
+define( 'CODISTOCONNECT_VERSION', '1.3.34' );
 define( 'CODISTOCONNECT_RESELLERKEY', '' );
 
 if ( ! class_exists( 'CodistoConnect' ) ) :
@@ -238,7 +238,7 @@ final class CodistoConnect {
 			@ob_clean();
 		}
 
-		if ( ! in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ))) {
+		if ( ! is_plugin_active( 'woocommerce/woocommerce.php' ) ) {
 			$this->sendHttpHeaders(
 				'500 Config Error',
 				array(
