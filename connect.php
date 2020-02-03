71014eb1b216ce1a7dbe415a13078a5d5d80053b
James Livas <jlivas@ontech.com.au>
2020-02-03 12:54:03 +1100

    woocommerce tested up to 3.9.1

--- connect.php
+++ connect.php
@@ -5,23 +5,23 @@
  * Description: WooCommerce Amazon & eBay Integration - Convert a WooCommerce store into a fully integrated Amazon & eBay store in minutes
  * Author: Codisto
  * Author URI: https://codisto.com/
- * Version: 1.3.36
+ * Version: 1.3.37
  * Text Domain: codisto-linq
  * Woo: 3545890:ba4772797f6c2c68c5b8e0b1c7f0c4e2
  * WC requires at least: 2.0.0
- * WC tested up to: 3.8.1
+ * WC tested up to: 3.9.1
  * License: GPLv2
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  *
  * @package Codisto LINQ by Codisto
- * @version 1.3.36
+ * @version 1.3.37
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-define( 'CODISTOCONNECT_VERSION', '1.3.36' );
+define( 'CODISTOCONNECT_VERSION', '1.3.37' );
 define( 'CODISTOCONNECT_RESELLERKEY', '' );
 
 if ( ! class_exists( 'CodistoConnect' ) ) :
