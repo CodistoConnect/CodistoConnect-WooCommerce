6add8e3f3ae43ee155b30f334e691abf71dfc40d
Deepak Dhiman <deepak.dhiman@monstar-lab.com.sg>
2021-01-19 14:50:53 +0530

    updates for stopping errors during order create for deleted orders

--- connect.php
+++ connect.php
@@ -5,7 +5,7 @@
  * Description: WooCommerce Amazon & eBay Integration - Convert a WooCommerce store into a fully integrated Amazon & eBay store in minutes
  * Author: Codisto
  * Author URI: https://codisto.com/
- * Version: 1.3.54
+ * Version: 1.3.55
  * Text Domain: codisto-linq
  * Woo: 3545890:ba4772797f6c2c68c5b8e0b1c7f0c4e2
  * WC requires at least: 2.0.0
@@ -14,14 +14,14 @@
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  *
  * @package Codisto LINQ by Codisto
- * @version 1.3.54
+ * @version 1.3.55
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-define( 'CODISTOCONNECT_VERSION', '1.3.54' );
+define( 'CODISTOCONNECT_VERSION', '1.3.55' );
 define( 'CODISTOCONNECT_RESELLERKEY', '' );
 
 if ( ! class_exists( 'CodistoConnect' ) ) :
@@ -1626,6 +1626,11 @@ final class CodistoConnect {
 
 								$tax += $line_total_tax;
 							} else {
+
+								if(!is_object($order)) {
+									continue;
+								}
+								
 								$order->remove_order_items( 'shipping' );
 
 								$item_id = wc_add_order_item(
