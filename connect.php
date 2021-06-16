a3a0b548e622fd028f21cd9ef03ea8bd5c0f0e97
Deepak Dhiman <deepak.dhiman@monstar-lab.com.sg>
2021-06-16 12:04:16 +0530

    updates for status header

--- connect.php
+++ connect.php
@@ -5,7 +5,7 @@
  * Description: Sell multichannel on Google, Amazon, eBay & Walmart direct from WooCommerce. Create listings & sync products, inventory & orders directly from WooCommerce
  * Author: Codisto
  * Author URI: https://codisto.com/
- * Version: 1.3.58
+ * Version: 1.3.59
  * Text Domain: codisto-linq
  * Woo: 3545890:ba4772797f6c2c68c5b8e0b1c7f0c4e2
  * WC requires at least: 2.0.0
@@ -14,14 +14,14 @@
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  *
  * @package Codisto LINQ by Codisto
- * @version 1.3.58
+ * @version 1.3.59
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-define( 'CODISTOCONNECT_VERSION', '1.3.58' );
+define( 'CODISTOCONNECT_VERSION', '1.3.59' );
 define( 'CODISTOCONNECT_RESELLERKEY', '' );
 
 if ( ! class_exists( 'CodistoConnect' ) ) :
@@ -162,7 +162,10 @@ final class CodistoConnect {
 			$_SERVER['REQUEST_URI'] = '/wp-admin'.$_SERVER['REQUEST_URI'];
 		}
 
-		status_header( $status );
+		preg_match_all('/((\d+)|(.*))/', $status, $statusArray);
+		$statusCode = isset($statusArray[0]) && isset($statusArray[0][0]) ? (int) $statusArray[0][0] : "";
+		$statusMessage = isset($statusArray[0]) && isset($statusArray[0][1]) ? trim($statusArray[0][1]) : "";
+		status_header( $statusCode, $statusMessage );
 		foreach ( $headers as $header => $value ) {
 			header( $header.': '.$value );
 		}
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: codisto
 Tags: ecommerce, e-commerce, woocommerce, ebay, amazon, paypal, integration, multi-channel, listings, store, sales, sell, shop
 Requires at least: 4.0
 Tested up to: 5.6.0
-Stable tag: 1.3.58
+Stable tag: 1.3.59
 Requires PHP: 5.6
 License: GPLv2
 License URI: https://www.gnu.org/licenses/gpl-2.0.txt
@@ -241,6 +241,9 @@ Yes, there are! Please check out our growing knowledgebase at <http://help.codis
 5. Fulfill your Google, Amazon and eBay orders directly from WooCommerce
 
 == Changelog ==
+
+= 1.3.59 - 16/06/2021 =
+* Update - status header updates
 
 = 1.3.58 - 09/06/2021 =
 * Update - prevent completed orders being set back to processing
